8ff5f3f74b0530cca0d937c71c2650f91d22b041
Rickard Svenningsson <rickard@infosective.se>
2021-04-25 00:04:46 +0200

    Tagit bort server service

--- serverservice/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo -- bash -c 'echo -e "HTTP/1.1 200 OK\r\nSuccessful" | nc -lN 81; reboot.sh'&
-sudo -- bash -c 'echo -e "HTTP/1.1 200 OK\r\nSuccessful" | nc -lN 82; shutdown.sh'&
-
