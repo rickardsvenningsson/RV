46e36abf56c002a4dc43bc415e7f966f989df8f5
Rickard Svenningsson <rickard@infosective.se>
2021-04-24 22:37:02 +0200

    Lagt till alternativ shutdown

--- clientservice/start.sh
+++ clientservice/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo -- bash -c 'echo -e "HTTP/1.1 200 OK\r\nSuccessful" | nc -lN 81; reboot' 
+sudo -- bash -c 'echo -e "HTTP/1.1 200 OK\r\nSuccessful" | nc -lN 81; reboot'&
+sudo -- bash -c 'echo -e "HTTP/1.1 200 OK\r\nSuccessful" | nc -lN 82; shutdown'&
+
