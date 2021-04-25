8751fa691508f9d806e2aea0b50e00522406368c
Rickard Svenningsson <rickard@infosective.se>
2021-04-25 20:27:28 +0200

    Lagt till timeout för kontroll

--- clientservice/start.sh
+++ clientservice/start.sh
@@ -5,7 +5,7 @@
 sudo -- bash -c 'echo -e "HTTP/1.1 200 OK\r\nSuccessful" | nc -lN 81; reboot'&
 sudo -- bash -c 'echo -e "HTTP/1.1 200 OK\r\nSuccessful" | nc -lN 82; shutdown -h now'&
 
-while [[ $(nc -zv homeassistant.local 8123 2>&1) != *"succeeded"* ]]; do
+while [[ $(nc -zv -w 5 homeassistant.local 8123 2>&1) != *"succeeded"* ]]; do
    sleep 10;
 done
 
