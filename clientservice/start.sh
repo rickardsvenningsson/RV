489625b29daa655c37e189cac5831960e1a196e9
Rickard Svenningsson <rickard@infosective.se>
2021-04-25 20:20:43 +0200

    lagt in credentials

--- clientservice/start.sh
+++ clientservice/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 sudo -- bash -c 'echo -e "HTTP/1.1 200 OK\r\nSuccessful" | nc -lN 81; reboot'&
 sudo -- bash -c 'echo -e "HTTP/1.1 200 OK\r\nSuccessful" | nc -lN 82; shutdown -h now'&
 
