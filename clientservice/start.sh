c16df17840ddf31a097d26127439ff920a12ca0e
Rickard Svenningsson <rickard@infosective.se>
2021-04-25 19:33:44 +0200

    Uppdaterat klienten

--- clientservice/start.sh
+++ clientservice/start.sh
@@ -3,3 +3,8 @@
 sudo -- bash -c 'echo -e "HTTP/1.1 200 OK\r\nSuccessful" | nc -lN 81; reboot'&
 sudo -- bash -c 'echo -e "HTTP/1.1 200 OK\r\nSuccessful" | nc -lN 82; shutdown -h now'&
 
+while [[ $(nc -zv homassistant.local 8123 2>&1) != *"succeeded"* ]]; do
+   sleep 10;
+done
+
+chromium-browser -kiosk file:///home/pi/RV/html/index.html
